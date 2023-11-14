ed1040948ac4e7d76ecefe169a06eb767bbddfd4
Ben Richards <therunningdev@gmail.com>
2023-11-14 13:58:49 +0100

    Added Workflow for XYplorer
    - added new xyplorer workflow
    - added new xyplorer PS script for testing
    - renamed sidequest.easyinstaller script

---
+++ xyplorer.ps1
@@ -0,0 +1,25 @@
+$versionUrl = 'https://www.xyplorer.com/version.php'
+
+# Install needed modules
+Install-Module Microsoft.WinGet.Client -Force
+
+# Get the current WinGet version of the package
+$winGetVersion = Find-WinGetPackage -Id $packageId | `
+    Select-Object -ExpandProperty version
+
+# Get the latest version
+$latestVersion = Invoke-WebRequest $versionUrl | Select-Object -ExpandProperty Content
+
+# Latest version and WinGet version are the same...
+if ($latestVersion -eq $winGetVersion) {
+    Write-Host "Released Version ($latestVersion) == WinGet Version ($winGetVersion)...Exiting"
+    return
+}
+
+<#
+# Call WinGetCreate to update the version and URL of the package
+& wingetcreate update $packageId `
+    --version $version `
+    --submit `
+    --token ${{secrets.PublicToken} }
+#>
