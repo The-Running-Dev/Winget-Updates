618b46aa2720df2e3df123b644369ac897001972
Ben Richards <therunningdev@gmail.com>
2023-11-14 13:41:36 +0100

    Cleanup and documentation

--- build.ps1
+++ build.ps1
@@ -1,34 +1,45 @@
 $owner = 'SideQuestVR'
 $repository = 'EasyInstallerReleases'
 $packageId = 'SideQuestVR.SideQuestEasyInstaller'
-$wingetCreateUrl = 'https://aka.ms/wingetcreate/latest'
+$installerRegEx = 'SideQuest-Setup-.*-x64-win.exe$'
+$gitHubUsername = 'The-Running-Dev'
 
+# Install needed modules
 Install-Module Microsoft.WinGet.Client -Force
 Install-Module PowerShellForGitHub -Force
 
-# Get the current Winget version of the package
-$wingetVersion = Find-WinGetPackage -Id $packageId | `
+# Setup the GitHub module, disable telemetry and set authentication
+Set-GitHubConfiguration -DisableTelemetry
+#$secureString = (${{secrets.PublicToken} } | ConvertTo-SecureString -AsPlainText -Force)
+#$credentials = New-Object System.Management.Automation.PSCredential $gitHubUsername, $secureString
+#Set-GitHubAuthentication -Credential $credentials
+
+# Get the current WinGet version of the package
+$winGetVersion = Find-WinGetPackage -Id $packageId | `
     Select-Object -ExpandProperty version
 
+# Get the latest release from the owner/repository
 $release = Get-GitHubRelease `
     -OwnerName $owner `
     -RepositoryName $repository `
     -Latest
-$version = $release.tag_name -replace 'v', ''
+$latestVersion = $release.tag_name -replace 'v', ''
 
-if ($version -eq $wingetVersion) {
-Write-Host "Released Version ($version) == Winget Version ($wingetVersion)...Exiting"
-return
+# Latest version and WinGet version are the same...
+if ($latestVersion -eq $winGetVersion) {
+    Write-Host "Released Version ($latestVersion) == WinGet Version ($winGetVersion)...Exiting"
+    return
 }
 
+# Get the installer download URL
 $downloadUrl = $release.assets | `
-    Where-Object name -Match 'SideQuest-Setup-.*-x64-win.exe$' | `
+    Where-Object name -Match $installerRegEx | `
     Select-Object -ExpandProperty browser_download_url
-
-Invoke-WebRequest $wingetCreateUrl -OutFile wingetcreate.exe
-
-.\wingetcreate.exe update $packageId `
---urls '$downloadUrl' '$downloadUrl' `
---version $version `
-#--submit `
-#--token ${{secrets.PublicToken}}
+<#
+# Call WinGetCreate to update the version and URL of the package
+& wingetcreate update $packageId `
+    --urls '$downloadUrl' '$downloadUrl' `
+    --version $version `
+    --submit `
+    --token ${{secrets.PublicToken} }
+#>
