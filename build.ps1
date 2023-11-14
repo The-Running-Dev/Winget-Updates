7b0fd57284c11a2904c8f2f23c30a493e7df13f0
Ben Richards <therunningdev@gmail.com>
2023-11-14 12:56:51 +0100

    Switched to local PS variables over ENV variables

--- build.ps1
+++ build.ps1
@@ -1,13 +1,18 @@
+$owner = 'SideQuestVR'
+$repository = 'EasyInstallerReleases'
+$packageId = 'SideQuestVR.SideQuestEasyInstaller'
+$wingetCreateUrl = 'https://aka.ms/wingetcreate/latest'
+
 Install-Module Microsoft.WinGet.Client
 Install-Module PowerShellForGitHub
 
 # Get the current Winget version of the package
-$wingetVersion = Find-WinGetPackage -Id $env:packageId | `
+$wingetVersion = Find-WinGetPackage -Id $packageId | `
     Select-Object -ExpandProperty version
 
 $release = Get-GitHubRelease `
-    -OwnerName $env:owner `
-    -RepositoryName $env:repository `
+    -OwnerName $owner `
+    -RepositoryName $repository `
     -Latest
 $version = $release.tag_name -replace 'v', ''
 
@@ -19,3 +24,11 @@ return
 $downloadUrl = $release.assets | `
     Where-Object name -Match 'SideQuest-Setup-.*-x64-win.exe$' | `
     Select-Object -ExpandProperty browser_download_url
+
+Invoke-WebRequest $wingetCreateUrl -OutFile wingetcreate.exe
+
+.\wingetcreate.exe update $packageId `
+--urls '$downloadUrl' '$downloadUrl' `
+--version $version `
+#--submit `
+#--token ${{secrets.PublicToken}}
