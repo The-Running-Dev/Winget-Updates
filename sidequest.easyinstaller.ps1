2cb20fb61287a449a5143eb804d7de63e7b2e48a
Ben Richards <The-Running-Dev@users.noreply.github.com>
2023-11-30 20:25:34 +0100

    Refactoring and Fixes
    - Moved  PowerShell into own scripts, instead of inline
    - Addressed issue with XYplorer update not working

--- sidequest.easyinstaller.ps1
+++ sidequest.easyinstaller.ps1
@@ -1,45 +1,83 @@
+[CmdletBinding(SupportsShouldProcess = $true)]
+Param(
+    [Parameter(Mandatory = $false)][string] $gitHubAccessToken
+)
+
 $owner = 'SideQuestVR'
 $repository = 'EasyInstallerReleases'
 $packageId = 'SideQuestVR.SideQuestEasyInstaller'
 $installerRegEx = 'SideQuest-Setup-.*-x64-win.exe$'
+
+$winGetVersion = '1.0'
+$latestVersion = '1.0'
+$whatIf = $WhatIfPreference.IsPresent
 $gitHubUsername = 'The-Running-Dev'
+# Use the token passed in as a paramater, or if empty, use the ENV token GitHubAccessToken
+$accessToken = @{$true = $gitHubAccessToken; $false = $env:GitHubAccessToken }["" -notmatch $gitHubAccessToken]
 
-# Install needed modules
-Install-Module Microsoft.WinGet.Client -Force
-Install-Module PowerShellForGitHub -Force
+if (-not $accessToken) {
+    Write-Warning "`GitHub Access Token Not Set...Exiting"
 
-# Setup the GitHub module, disable telemetry and set authentication
-Set-GitHubConfiguration -DisableTelemetry
-#$secureString = (${{secrets.PublicToken} } | ConvertTo-SecureString -AsPlainText -Force)
-#$credentials = New-Object System.Management.Automation.PSCredential $gitHubUsername, $secureString
-#Set-GitHubAuthentication -Credential $credentials
-
-# Get the current WinGet version of the package
-$winGetVersion = Find-WinGetPackage -Id $packageId | `
-    Select-Object -ExpandProperty version
-
-# Get the latest release from the owner/repository
-$release = Get-GitHubRelease `
-    -OwnerName $owner `
-    -RepositoryName $repository `
-    -Latest
-$latestVersion = $release.tag_name -replace 'v', ''
-
-# Latest version and WinGet version are the same...
-if ($latestVersion -eq $winGetVersion) {
-    Write-Host "Released Version ($latestVersion) == WinGet Version ($winGetVersion)...Exiting"
     return
 }
 
-# Get the installer download URL
-$downloadUrl = $release.assets | `
-    Where-Object name -Match $installerRegEx | `
-    Select-Object -ExpandProperty browser_download_url
-<#
+# Install needed modules
+if (-not (Get-Module Microsoft.WinGet.Client) -and (-not $whatIf)) {
+    Install-Module Microsoft.WinGet.Client -Force
+}
+
+if (-not (Get-Module PowerShellForGitHub) -and (-not $whatIf)) {
+    Install-Module PowerShellForGitHub -Force
+}
+
+# Setup the GitHub module, disable telemetry and set authentication
+Set-GitHubConfiguration -DisableTelemetry
+$secureString = ($accessToken | ConvertTo-SecureString -AsPlainText -Force)
+$credentials = New-Object System.Management.Automation.PSCredential $gitHubUsername, $secureString
+Set-GitHubAuthentication -Credential $credentials
+
+# Get the current WinGet version of the package
+if ($PSCmdlet.ShouldProcess("-Id $packageId", "Find-WinGetPackage")) {
+    $winGetVersion = Find-WinGetPackage -Id $packageId -MatchOption Equals | `
+        Select-Object -ExpandProperty version
+}
+
+# Get the latest release from the owner/repository
+# Get the latest version
+if ($PSCmdlet.ShouldProcess("-OwnerName $owner -RepositoryName $repository -Latest", "Get-GitHubRelease")) {
+    $release = Get-GitHubRelease `
+        -OwnerName $owner `
+        -RepositoryName $repository `
+        -Latest
+
+    # Get the latest version from the tag_name
+    $latestVersion = $release.tag_name -replace 'v', ''
+
+    # Get the installer URL
+    $installerUrl = $release.assets | `
+        Where-Object name -Match $installerRegEx | `
+        Select-Object -ExpandProperty browser_download_url
+}
+
+# WinGet version does not exist...
+if (-not $winGetVersion -and (-not $whatIf)) {
+    Write-Warning "Package Does Not Exist on WinGet...Exiting"
+
+    return
+}
+
+# Latest version and WinGet version are the same...
+if (($latestVersion -eq $winGetVersion) -and (-not $whatIf)) {
+    Write-Warning "Latest Version ($latestVersion) == WinGet Version ($winGetVersion)...Exiting"
+
+    return
+}
+
 # Call WinGetCreate to update the version and URL of the package
-& wingetcreate update $packageId `
-    --urls '$downloadUrl' '$downloadUrl' `
-    --version $version `
-    --submit `
-    --token ${{secrets.PublicToken} }
-#>
+if ($PSCmdlet.ShouldProcess("$packageId --version $latestVersion --urls '$installerUrl $installerUrl'", "wingetcreate update")) {
+    & wingetcreate update $packageId `
+        --version $latestVersion `
+        --urls "$installerUrl $installerUrl" `
+        --submit `
+        --token $accessToken
+}
--- xyplorer.ps1
+++ xyplorer.ps1
@@ -1,65 +1,62 @@
-$versionUrl = 'https://www.xyplorer.com/version.php'
+[CmdletBinding(SupportsShouldProcess = $true)]
+Param(
+    [Parameter(Mandatory = $false)][string] $gitHubAccessToken
+)
+
 $packageId = 'CologneCodeCompany.XYplorer'
-$versionUrl = 'https://www.xyplorer.com/version.php'
-$downloadUrl = 'https://www.xyplorer.com/download/xyplorer_full.zip'
+$getVersionUrl = 'https://www.xyplorer.com/version.php'
+$getInstallerUrl = 'https://www.xyplorer.com/version.php?installer=1'
+
+$winGetVersion = '1.0'
+$latestVersion = '1.0'
+$whatIf = $WhatIfPreference.IsPresent
+# Use the token passed in as a paramater, or if empty, use the ENV token GitHubAccessToken
+$accessToken = @{$true = $gitHubAccessToken; $false = $env:GitHubAccessToken }["" -notmatch $gitHubAccessToken]
+
+if (-not $accessToken) {
+    Write-Warning "`GitHub Access Token Not Set...Exiting"
+
+    return
+}
 
 # Install needed modules
-Install-Module Microsoft.WinGet.Client -Force
+if (-not (Get-Module Microsoft.WinGet.Client) -and (-not $whatIf)) {
+    Install-Module Microsoft.WinGet.Client -Force
+}
 
 # Get the current WinGet version of the package
-$winGetVersion = Find-WinGetPackage -Id $packageId -MatchOption Equals | `
-    Select-Object -ExpandProperty version
+if ($PSCmdlet.ShouldProcess("-Id $packageId", "Find-WinGetPackage")) {
+    $winGetVersion = Find-WinGetPackage -Id $packageId -MatchOption Equals | `
+        Select-Object -ExpandProperty version
+}
 
 # Get the latest version
-$latestVersion = Invoke-WebRequest $versionUrl | Select-Object -ExpandProperty Content
+if ($PSCmdlet.ShouldProcess($getVersionUrl, "Invoke-WebRequest")) {
+    $latestVersion = Invoke-WebRequest $getVersionUrl | Select-Object -ExpandProperty Content
+}
 
 # WinGet version does not exist...
-if (-not $winGetVersion) {
+if (-not $winGetVersion -and (-not $whatIf)) {
     Write-Warning "Package Does Not Exist on WinGet...Exiting"
+
     return
 }
 
 # Latest version and WinGet version are the same...
-if ($latestVersion -eq $winGetVersion) {
+if (($latestVersion -eq $winGetVersion) -and (-not $whatIf)) {
     Write-Warning "Latest Version ($latestVersion) == WinGet Version ($winGetVersion)...Exiting"
+
     return
 }
 
-$zipFile = Join-Path . (Split-Path -Leaf $downloadUrl)
-Invoke-WebRequest $downloadUrl -out $zipFile
-
-[Reflection.Assembly]::LoadWithPartialName('System.IO.Compression.FileSystem')
-$RelativeFilePath = [IO.Compression.ZipFile]::OpenRead($zipFile).Entries | `
-    Where-Object Name -Match '.exe' | `
-    Select-Object -ExpandProperty Name
-
-$winGetCreate = New-Object System.Diagnostics.ProcessStartInfo;
-$winGetCreate.FileName = 'wingetcreate'
-$winGetCreate.UseShellExecute = $false
-$winGetCreate.RedirectStandardInput = $true
-$winGetCreate.Arguments = @(
-    "update $packageId"
-    "--version $latestVersion"
-    "--urls '$downloadUrl|x64|machine'"
-    "--interactive"
-    "--submit"
-    "--token $env:PublicAccessToken"
-)
-
-Write-Debug ($winGetCreate.Arguments | ConvertTo-Json)
-
-$process = [System.Diagnostics.Process]::Start($winGetCreate)
-
-Start-Sleep -s 10
-
-$process.StandardInput.WriteLine($RelativeFilePath)
+# Get the installer URL
+$installerUrl = Invoke-WebRequest $getInstallerUrl | Select-Object -ExpandProperty Content
 
 # Call WinGetCreate to update the version and URL of the package
-<#
-          & wingetcreate update $packageId `
-            --version $latestVersion `
-            --urls "$downloadUrl|x64|machine|$RelativeFilePath" `
-            --interactive `
-            --submit `
-            --token $env:PublicAccessToken'
-          #>
+if ($PSCmdlet.ShouldProcess("$packageId --version $latestVersion --urls '$installerUrl|x64|machine'", "wingetcreate update")) {
+    & wingetcreate update $packageId `
+        --version $latestVersion `
+        --urls "$installerUrl|x64|machine" `
+        --submit `
+        --token $accessToken
+}
