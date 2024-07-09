48632457e0feeb6477e7f8fe6c471b6ba16f6d16
Ben Richards <The-Running-Dev@users.noreply.github.com>
2024-07-09 22:22:57 +0200

    Updated Portable Script
    - New function Update-WithYamlCreate to pull and use YamlCreate
    - Updated xyplorer.portable to use YamlCreate function

---
+++ Common/Update-PackageWithClone.ps1
@@ -0,0 +1,85 @@
+<#
+========================================================
+Example Parameters
+========================================================
+PackageId           = 'CologneCodeCompany.XYplorer'
+VersionUrl          = 'https://www.xyplorer.com/version.php'
+WinGetRepository    = $global:WingetRepositoryName
+WinGetOwner         = $global:WingetRepositoryOwner
+RepositoryUrl       = 'https://'
+GitHubUsername      = $global:gitHubUsername
+AaccessToken        = $accessToken
+#>
+function Update-WithYamlCreate {
+    [CmdletBinding(SupportsShouldProcess = $true)]
+    param(
+        [hashtable] $parameters
+    )
+
+    $updateParameters = @('-Mode 2')
+    $updateParameters += '-AutoUpgrade'
+    $updateParameters += '-PackageIdentifier'
+    $updateParameters += $parameters.PackageId
+
+    Exit-WithWarning `
+        -Condition (-not $parameters.AccessToken) `
+        -Message 'GitHub Access Token Not Set...Exiting'
+
+    Exit-WithWarning `
+        -Condition (-not (Get-Command 'git' -ErrorAction SilentlyContinue)) `
+        -Message 'Git Not Installed...Exiting'
+
+    # Install needed modules
+    Install-ModuleSafe Microsoft.WinGet.Client
+    Install-ModuleSafe PowerShellForGitHub
+
+    # Setup GitHub connection with the PAT token
+    Set-GitHubConnection $parameters.GitHubUsername $parameters.AccessToken -WhatIf:$WhatIfPreference
+
+    # Get the current WinGet version of the package
+    $winGetVersion = Get-WinGetVersion $parameters.PackageId -WhatIf:$WhatIfPreference
+
+    # Get the latest version
+    $latestVersion = Get-ContentFromUrl $parameters.VersionUrl -WhatIf:$WhatIfPreference
+
+    if (-not $parameters.SkipVersionCheck) {
+        # Latest version and WinGet version are the same...
+        Exit-WithWarning `
+            -Condition ($latestVersion -eq $winGetVersion) `
+            -Message "Latest Version ($latestVersion) == WinGet Version ($winGetVersion)...Exiting"
+    }
+
+    if (-not $parameters.SkipPRCheck) {
+        # Check for existing pull request for this package
+        Test-WinGetPullRequest -packageId $parameters.PackageId -owner $parameters.WinGetOwner -WhatIf:$WhatIfPreference
+    }
+
+    $updateParameters += '-PackageVersion'
+    $updateParameters += $latestVersion
+
+    if ($PSCmdlet.ShouldProcess($parameters.YamlCreateRepositoryUrl, 'git clone')) {
+        if (-not $parameters.SkipClone) {
+            if (Test-Path $parameters.YamlCreateRepositoryDir) {
+                Write-Warning "$($parameters.YamlCreateRepositoryDir) Exists...Deleting"
+
+                Remove-Item -Recurse -Force $parameters.YamlCreateRepositoryDir
+            }
+
+            & git clone $parameters.YamlCreateRepositoryUrl $parameters.YamlCreateRepositoryDir
+        }
+    }
+
+    $yamlCreateScript = "$($parameters.YamlCreateRepositoryDir)\Tools\YamlCreate.ps1"
+
+    if ($PSCmdlet.ShouldProcess($yamlCreateScript, $updateParameters)) {
+        if (Test-Path $parameters.YamlCreateRepositoryDir) {
+            if (-not $parameters.SkipSubmit) {
+                # Call YamlCreate to update the package
+                & $yamlCreateScript $updateParameters
+            }
+            else {
+                Write-Output "$yamlCreateScript $updateParameters"
+            }
+        }
+    }
+}
--- xyplorer.portable.ps1
+++ xyplorer.portable.ps1
@@ -2,6 +2,7 @@
 Param(
     [Parameter()][string] $gitHubAccessToken,
     [Parameter()][switch] $skipVersionCheck,
+    [Parameter()][switch] $skipClone,
     [Parameter()][switch] $skipPRCheck,
     [Parameter()][switch] $skipSubmit
 )
@@ -11,14 +12,17 @@ Import-Module (Join-Path $PSScriptRoot 'Common\Common.psm1') -Force
 # Use the token passed in as a paramater, or if empty, use the ENV token GitHubAccessToken
 $accessToken = @{$true = $gitHubAccessToken; $false = $env:GitHubAccessToken }["" -notmatch $gitHubAccessToken]
 
-Get-WinGetPackageUpdate @{
-    PackageId           = 'CologneCodeCompany.XYplorerPortable'
-    VersionUrl          = 'https://www.xyplorer.com/version.php'
-    WinGetOwner         = $global:WingetRepositoryOwner
-    WinGetRepository    = $global:WingetRepositoryName
-    GitHubUsername      = $global:gitHubUsername
-    AccessToken         = $accessToken
-    SkipVersionCheck    = $skipVersionCheck.IsPresent
-    SkipPRCheck         = $skipPRCheck.IsPresent
-    SkipSubmit          = $skipSubmit.IsPresent
+Update-WithYamlCreate @{
+    PackageId               = 'CologneCodeCompany.XYplorerPortable'
+    VersionUrl              = 'https://www.xyplorer.com/version.php'
+    YamlCreateRepositoryUrl = 'https://github.com/The-Running-Dev/Winget-Packages.git'
+    YamlCreateRepositoryDir = Join-Path $PSScriptRoot 'Winget-Packages'
+    WinGetOwner             = $global:WingetRepositoryOwner
+    WinGetRepository        = $global:WingetRepositoryName
+    GitHubUsername          = $global:gitHubUsername
+    AccessToken             = $accessToken
+    SkipClone               = $skipClone.IsPresent
+    SkipVersionCheck        = $skipVersionCheck.IsPresent
+    SkipPRCheck             = $skipPRCheck.IsPresent
+    SkipSubmit              = $skipSubmit.IsPresent
 } -WhatIf:$WhatIfPreference
